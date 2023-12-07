c6a0b6b50004a4db034c7c00ca4d111f93c88cf6
Rylan Sturm <rylan.sturm@gmail.com>
2023-12-06 23:28:21 -0700

    hello world in actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/index.html")]
+async fn index() -> impl Responder {
+    format!("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::redirect("/", "/index.html"))
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
