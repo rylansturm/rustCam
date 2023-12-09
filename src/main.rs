4e195ba2e413a865714cde77608f14ed5cdf8dbb
Rylan Sturm <rylan.sturm@gmail.com>
2023-12-08 17:46:54 -0700

    decided not to use gstreamer because I'm banging my head against a wall. Introductin rscam! An image can now be served at /image

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
 #![warn(clippy::pedantic)]
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder, http::header};
+use rscam::{Camera, Config};
+use std::{fs, io::Write};
+use futures::{stream, StreamExt};
 
 
 #[get("/index.html")]
 async fn index() -> impl Responder {
     "Hello, world!".to_string()
+}
+
+// #[get("/video")]
+// async fn video() -> impl Responder {
+//     let boundary = "frame";
+
+//     HttpResponse::OK()
+//         .content_type(format!("multipart/x-mixed-replace;boundary={}", boundary))
+//         .streaming(stream:unfold())
+// }
+
+#[get("/image")]
+async fn image() -> impl Responder {
+    let mut camera = rscam::new("/dev/video0").unwrap();
+    camera.start(&Config{
+        interval: (1, 30),
+        resolution: (1280, 720),
+        format: b"MJPG",
+        ..Default::default()
+    }).unwrap();
+    let frame = camera.capture().unwrap();
+    let image_data = frame.to_vec();
+
+    HttpResponse::Ok()
+        .content_type("image/jpeg")
+        .body(image_data)
 }
 
 
@@ -15,8 +44,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(web::redirect("/", "/index.html"))
             .service(index)
+            .service(image)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
